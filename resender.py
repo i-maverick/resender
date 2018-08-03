1201824259b4987b4c57ac6583338f4bdc0b1779
i-maverick <in79@mail.ru>
2018-08-03 18:10:59 +0300

    Update resender.py

--- resender.py
+++ resender.py
@@ -7,6 +7,12 @@ HOST = '127.0.0.1'
 
 
 class Resender:
+    SourceType = {
+        Initiator,
+        Receiver,
+        Stub
+    }
+    
     def __init__(self, data, loop):
         self.id = data['id']
         self.name = data['name']
@@ -20,8 +26,8 @@ class Resender:
         self.running = True
         self.loop = loop
 
-#         self.sv_writer = None
-#         self.ts_writer = None
+        self.initiator_writer = None
+        self.receiver_writer = None
 #         self.stub_writer = None
 
         self.setup_logger()
@@ -40,14 +46,7 @@ class Resender:
     async def prepare_response(data):
         return data
 
-    async def initiator_connected(self, reader, writer):
-#         if is_ts:
-#             self.ts_writer = writer
-#             print('TS connected')
-#         else:
-#             self.stub_writer = writer
-#             print('Stub connected')
-
+    async def main_loop(self, reader, writer)
         while self.running:
             data = await reader.read(READ_BUFFER)
             if data:
@@ -55,46 +54,26 @@ class Resender:
 
                 response = await self.prepare_response(data)
 
-                self.sv_writer.write(response)
-                self.sv_writer.drain()
+                self.writer.write(response)
+                self.writer.drain()
                 self.log.debug('sent {!r}'.format(response.decode('utf-8')))
             else:
                 self.log.debug('connection closing')
+                self.writer.close()
                 self.running = False
 
-    async def connect_initiator():
-        await asyncio.start_server(self.initiator_connected, HOST, self.initiator_port, self.loop)
-#         await asyncio.start_server(lambda r, w: self.connected(r, w, False),
-#                                    HOST, self.port_stub, loop=self.loop)
-
-    async def connect_receiver():
+    async def initiator_connected(self, initiator_reader, initiator_writer):
         try:
-            reader, writer = await asyncio.open_connection(HOST, self.port_receiver, self.loop)
+            receiver_reader, receiver_writer = await asyncio.open_connection(HOST, self.port_receiver, self.loop)
+            await self.main_loop(receiver_reader, initiator_writer)  # receiver to initiator
+            await self.main_loop(initiator_reader, receiver_writer)  # initiator to receiver
         except (asyncio.TimeoutError, ConnectionRefusedError) as err:
-            print('Error connecting to receiver: {}'.format(err))
+            print(f'Connection error: {err}')
+            self.running = False
 
     async def start(self):
-        await self.connect_initiator()
-        await self.connect_receiver()
-
-    async def main_loop(self):
-        while self.running:
-            try:
-                data = await sv_reader.read(READ_BUFFER)
-                if data:
-                    self.log.debug('received {!r}'.format(data.decode('utf-8')))
-
-                    response = await self.prepare_response(data)
-
-                    self.ts_writer.write(response)
-                    self.ts_writer.drain()
-                    self.log.debug('sent {!r}'.format(response.decode('utf-8')))
-                else:
-                    self.log.debug('connection closing')
-                    self.running = False
-            except Exception as err:
-                print(f'Error: {err}')
-                self.running = False
+        self.running = True
+        await asyncio.start_server(self.initiator_connected, HOST, self.initiator_port, loop=self.loop)
 
     async def stop(self):
         self.running = False
